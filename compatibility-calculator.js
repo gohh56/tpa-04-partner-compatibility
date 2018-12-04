55a5f993bd860e1d51762c36caa0f34f28408c19
gohh56 <ystokio@gmail.com>
2018-12-05 02:17:32 +0900

    fix bug

--- compatibility-calculator.js
+++ compatibility-calculator.js
@@ -7,8 +7,8 @@ const calculateBestMatch = function(quizSubmissions) {
 
   // set closest match to 1st candidate
   let closestMatch = candidates[0];
-  let prePoints = 0;
-  let points = 0;
+  let currentPersonPointCount = 0;
+  let maxPointCount = 0;
 
   // for each candidate
   //  for each question
@@ -21,17 +21,17 @@ const calculateBestMatch = function(quizSubmissions) {
     for (let j = 2; j < candidates[i].length; j += 1) {
       for (let k = 0; k < candidates[i][j].length; k += 1) {
         if (candidates[i][j][k].value === quizSubmissions[j][k].value) {
-          points += 2;
+          currentPersonPointCount += 2;
         } else if (Math.abs(candidates[i][j][k].value - quizSubmissions[j][k].value) === 1) {
-          points += 1;
+          currentPersonPointCount += 1;
         }
       }
     }
-    if (points >= prePoints) {
+    if (currentPersonPointCount >= maxPointCount) {
       closestMatch = candidates[i];
+      maxPointCount = currentPersonPointCount;
     }
-    prePoints = points;
-    points = 0;
+    currentPersonPointCount = 0;
   }
 
   // for ties, the last person who has the highest score is returned
